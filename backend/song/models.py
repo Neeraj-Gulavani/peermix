895db9e409eb1e9217364f4a5991f48033a61019
Neeraj-Gulavani <neerajgulavani@gmail.com>
2025-05-18 00:12:50 +0530

    Created Playlist, Song and User Database Models

--- backend/song/models.py
+++ backend/song/models.py
@@ -1,3 +1,43 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Playlist(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True, editable=False)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = uuid.uuid4().hex[:8].upper()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+################################################
+
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    playlist = models.ForeignKey(Playlist, related_name='members', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('name', 'playlist')
+    def __str__(self):
+        return self.name
+
+################################################
+
+class Song(models.Model):
+    playlist = models.ForeignKey(Playlist, related_name='songs', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    songid = models.CharField(max_length=100)
+    added_by = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+
+    class Meta:
+        unique_together = ('title', 'author', 'playlist')
+    def geturl(self):
+        return f"https://www.youtube.com/watch?v={self.songid}"
+    def __str__(self):
+        return f"{self.title} by {self.artist}"
